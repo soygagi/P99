1a2058008eeeddce057987666b6f98c1ca5219eb
soygagi <101847092+soygagi@users.noreply.github.com>
2022-08-11 19:51:42 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -9,17 +9,24 @@ recognition.onresult = function(event){
     var Content = event.results[0][0].transcript;
     console.log(Content);
     document.getElementById("textbox").innerHTML = Content;
-    console.log(Content);
-    speak();
+    if(Content=="toma mi selfie"){
+        console.log("tomando mi selfie...");
+        speak();
+    }
+    
 }   
 function speak(){
     var synth = window.speechSynthesis;
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "tomando tu selfie en 5 segunda..";
+    //speak_data = document.getElementById("textbox").value;
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
     Webcam.attach(camera);
+    setTimeout(function(){
+        take_snapshot();
+        save();
+    },5000);
 }
-
 camera = document.getElementById("camera");
 Webcam.set({
 width:360,
@@ -27,3 +34,14 @@ height:250,
 image_format: 'png',
 png_quality:90
 });
+function take_snapshot(){
+    webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+function save(){
+    link =document.getElementById("link");
+    imgae = document.getElementById("Selfie_image").src;
+    link.href = image;
+    link.click();
+}
